28940000bf32605f4f26d351ed24df44642cdea9
Alberto Fabian <lf_alberto@hotmail.com>
2024-03-07 20:57:28 -0600

    ok

--- api/v1/index.php
+++ api/v1/index.php
@@ -1,23 +1,22 @@
 <?php
+
 require(__DIR__ . '/../../config.php');
 require(__DIR__ . '/../vendor/autoload.php');
+
+require(__DIR__ . '/../../app/modules/users/userscontroller.php');
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 $app = AppFactory::create();
-$app->setBasePath("/" . FOLDER . "api/v1"); // /myapp/api is the api folder (http://domain/myapp/api)
+$app->setBasePath("/" . FOLDER . "api/v1"); // /myapp/api es la carpeta api (http://domain/myapp/api)
 
+// Middleware de manejo de errores
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
 
-$app->get('/blog', function (Request $requets, Response $response, $args) {
-    $response->getBody()->write('Pagina principal a wuebo');
-    return  $response;
-});
-// Run app
-try {
-    $app->run();
-} catch (Exception $e) {
-    // We display a error message
-    die(json_encode(array("status" => "failed", "message" => "This action is not allowed")));
-}
+// Rutas
+require __DIR__ . '/routes.php';
+
+// Ejecutar la aplicación
+$app->run();
---
+++ api/v1/routes.php
@@ -0,0 +1,11 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+$app->get('/blog', function (Request $request, Response $response, $args) {
+    $response->getBody()->write('Página principal a wuebo');
+    return $response;
+});
+
+$app->get('/users', UserController::class . ':list');
