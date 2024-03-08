158e90c3d1cbfae672ae5d4efd0c10e051a7ef45
Alberto Fabian <lf_alberto@hotmail.com>
2024-03-07 19:27:51 -0600

    ok

--- api/v1/index.php
+++ api/v1/index.php
@@ -1,14 +1,13 @@
 <?php
-require (__DIR__ . '/../../config.php');
+require(__DIR__ . '/../../config.php');
 require(__DIR__ . '/../vendor/autoload.php');
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
-
 $app = AppFactory::create();
-$app->setBasePath("/".FOLDER."api/v1"); // /myapp/api is the api folder (http://domain/myapp/api)
+$app->setBasePath("/" . FOLDER . "api/v1"); // /myapp/api is the api folder (http://domain/myapp/api)
 
 
 $app->get('/blog', function (Request $requets, Response $response, $args) {
