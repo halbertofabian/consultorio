3c1d90accbae14dae1383706d917b5f2014bcef6
Your Name <victor081196>
2024-04-05 09:07:31 -0600

    Actividades 05-04-24

--- app/components/topbar.php
+++ app/components/topbar.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-light navbar-glass navbar-top navbar-expand">
 
     <button class="btn navbar-toggler-humburger-icon navbar-toggler me-1 me-sm-3" type="button" data-bs-toggle="collapse" data-bs-target="#navbarVerticalCollapse" aria-controls="navbarVerticalCollapse" aria-expanded="false" aria-label="Toggle Navigation"><span class="navbar-toggle-icon"><span class="toggle-line"></span></span></button>
-    <a class="navbar-brand me-1 me-sm-3" href="index.html">
+    <a class="navbar-brand me-1 me-sm-3" href="<?= HTTP_HOST ?>">
         <div class="d-flex align-items-center"><img class="me-2" src="<?= isset($_SESSION['scl']['ctr_logo']) ? $_SESSION['scl']['ctr_logo'] : "" ?>" alt="" width="150" /><span class="font-sans-serif"></span>
         </div>
     </a>
